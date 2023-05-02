b0013832717b3ba38f9efbcc7e6c434fe01445e3
Ridoy Sharif <developersridoy@gmail.com>
2023-05-02 18:52:21 +0600

    added new recipes pizza and some details

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get("/chef", (req, res) => {
   res.send(chefData);
 });
 
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const selectId = chefData.find((chef) => chef.id === id);
+  res.send(selectId);
+});
+
 app.listen(port, () => {
   console.log("hello chef ok", port);
 });
